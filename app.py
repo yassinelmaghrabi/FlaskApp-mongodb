2eb3a54d487824860e91c29d62e4178e47958baf
yassinelmaghrabi <yassin.the.mega2@gmail.com>
2024-12-03 18:56:38 +0200

    removed weird ahh comments

--- app.py
+++ app.py
@@ -3,20 +3,19 @@ from pymongo import MongoClient
 
 app = Flask(__name__)
 
-# MongoDB connection setup
-client = MongoClient("mongodb://root:example@mongodb:27017")  # Update credentials and URL if needed
-db = client["user_database"]  # Create a database
-collection = db["user_data"]  # Create a collection
+client = MongoClient("mongodb://root:example@mongodb:27017") 
+db = client["user_database"]  
+collection = db["user_data"]  
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        # Get form data
+
         name = request.form['name']
         email = request.form['email']
         age = request.form['age']
         
-        # Insert data into MongoDB
+
         collection.insert_one({"name": name, "email": email, "age": age})
         return redirect('/view')
     
@@ -24,7 +23,7 @@ def index():
 
 @app.route('/view')
 def view_data():
-    # Retrieve all data from the collection
+
     data = collection.find()
     return render_template('view.html', data=data)
 
